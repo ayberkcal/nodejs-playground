3a56218fe8c964f2f979253aa936850fef2ee41a
Ayberk <a.cal@mrd.com.tr>
2016-10-27 09:49:30 +0300

    commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/json', function(req, res, next) {
+  res.send({'asd': 'bds'});
+});
+
+exports.before = function(req, res, next){
+  var id = req.params.user_id;
+  if (!id) return next();
+  // pretend to query a database...
+  process.nextTick(function(){
+    req.user = db.users[id];
+    // cant find that user
+    if (!req.user) return next('route');
+    // found it, move on to the routes
+    next();
+  });
+};
+
 module.exports = router;
